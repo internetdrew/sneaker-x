186cc3a6115d5308969dc86edbfb7df187bd29c5
internetdrew <andrew@internetdrew.com>
2023-02-02 08:13:21 -0500

    add new schemas

---
+++ phanox-ecomm-backend/schemas/banner.js
@@ -0,0 +1,58 @@
+export default {
+  name: 'banner',
+  title: 'Banner',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {hotspot: true},
+    },
+    {
+      name: 'buttonText',
+      title: 'ButtonText',
+      type: 'string',
+    },
+    {
+      name: 'product',
+      title: 'Product',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'smallText',
+      title: 'SmallText',
+      type: 'string',
+    },
+    {
+      name: 'midText',
+      title: 'Midtext',
+      type: 'string',
+    },
+    {
+      name: 'largeText1',
+      title: 'LargeText1',
+      type: 'string',
+    },
+    {
+      name: 'largeText2',
+      title: 'LargeText2',
+      type: 'string',
+    },
+    {
+      name: 'discount',
+      title: 'Discount',
+      type: 'string',
+    },
+    {
+      name: 'salesTime',
+      title: 'SaleTime',
+      type: 'string',
+    },
+  ],
+}
