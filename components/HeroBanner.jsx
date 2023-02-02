96f8e54f6832fcff33024557f61b03980858cc1a
internetdrew <andrew@internetdrew.com>
2023-02-02 12:33:34 -0500

    add client for backend connect

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src='' alt='testing' className='hero-banner-image' />
+        <div>
+          <Link href='/product/ID'>
+            <button type='button'>BUTTON TEXT</button>
+            <div className='desc'>
+              <h5>Description</h5>
+              <p>DESCRIPTION</p>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { client } from '../library/client.js';
+import { Product, FooterBanner, HeroBanner } from '../components';
+
 const Home = () => {
   return (
     <>
-      Hero Banner
+      <HeroBanner />
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
@@ -14,6 +17,18 @@ const Home = () => {
       Footer
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
