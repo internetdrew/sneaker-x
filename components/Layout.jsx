7a26adbe9e7365e54d2dfb49dfd3ed8130195cee
internetdrew <andrew@internetdrew.com>
2023-02-02 15:55:23 -0500

    add cart item quantity

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Head from 'next/head';
+import { Footer, Navbar } from '.';
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>Sneakers X</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
