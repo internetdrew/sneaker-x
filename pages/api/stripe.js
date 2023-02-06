164c9c4cd21aba4fe3ed2b6e3b17f9acf2e1a81e
internetdrew <andrew@internetdrew.com>
2023-02-06 17:03:40 -0500

    add stripe shipping options

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,3 +1,38 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe();
+const stripe = new Stripe(process.env.NEXT_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_mthod_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1MYcHkFI7xSDre26zSaKt7fT' },
+          { shipping_rate: 'shr_1MYcKeFI7xSDre26Cbr80HD6' },
+        ],
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: '{{PRICE_ID}}',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
